dc3cece798c7e4e00a58c039473c51019b8ac90e
Luis <randi_junior144@hotmail.es>
2022-01-22 00:58:47 -0500

    --Servicio terminado

---
+++ src/main/java/com/entelgy/app/models/service/UserService.java
@@ -0,0 +1,10 @@
+package com.entelgy.app.models.service;
+
+import java.util.List;
+
+import com.entelgy.app.models.entity.User;
+
+public interface UserService {
+ 
+	public List<User> listaUsuarios();
+}
